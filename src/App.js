2f625df379b12b19c430763ac8de5cd0129e3e28
Aniket <aniketdhokane2071@gmail.com>
2022-02-26 18:09:50 +0530

    Toast Notification basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+
+toast.configure()
 
 function App() {
+
+  const notify=()=>{
+    toast("Basic notifications!")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={notify}>Notify!</button>
+      
     </div>
   );
 }
+
 
 export default App;
