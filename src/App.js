54ceff0502e58cc0cd5bf4c98c6dd9b852959be3
Aniket <aniketdhokane2071@gmail.com>
2022-02-26 18:16:17 +0530

    Type of toast notification

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
 
   const notify=()=>{
     toast("Basic notifications!",{ position: toast.POSITION.TOP_LEFT})
-    toast("Basic notifications!",{ position: toast.POSITION.TOP_CENTER})
-    toast("Basic notifications!",{ position: toast.POSITION.TOP_RIGHT})
-    toast("Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
-    toast("Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
+    toast.success("Sucess Basic notifications!",{ position: toast.POSITION.TOP_CENTER})
+    toast.info(" Info Basic notifications!",{ position: toast.POSITION.TOP_RIGHT})
+    toast.warn("Warning Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
+    toast.error("Error Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
     toast("Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
   }
 
