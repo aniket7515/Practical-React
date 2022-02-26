511a9efc53f17f8e76f89602ee5d2d5e0e418cff
Aniket <aniketdhokane2071@gmail.com>
2022-02-26 18:36:15 +0530

    Custom component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import {toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
+const CustomToast = ({closeToast})=>{
+  return(
+    <div>
+      Something went wrong!
+      <button onClick={closeToast}>Close</button>
+    </div>
+  )
+}
+
+
 toast.configure()
 
 function App() {
@@ -12,7 +22,7 @@ function App() {
     toast("Basic notifications!",{ position: toast.POSITION.TOP_LEFT})
     toast.success("Sucess Basic notifications!",{ position: toast.POSITION.TOP_CENTER,autoClose:8000})
     toast.info(" Info Basic notifications!",{ position: toast.POSITION.TOP_RIGHT,autoClose:false})
-    toast.warn("Warning Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
+    toast.warn(<CustomToast/>,{ position: toast.POSITION.BOTTOM_LEFT})
     toast.error("Error Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
     toast("Basic notifications!",{ position: toast.POSITION.BOTTOM_LEFT})
   }
