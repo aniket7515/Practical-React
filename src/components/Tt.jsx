198db081dc1c740376e51996199a5d3512888942
Aniket <aniketdhokane2071@gmail.com>
2022-02-27 15:19:36 +0530

    Variations on tippy component

--- src/components/Tt.jsx
+++ src/components/Tt.jsx
@@ -3,15 +3,37 @@ import Tippy from '@tippy.js/react'
 import '../App.css'
 import 'tippy.js/dist/tippy.css'
 
+const ColoredTooltip=()=>{
+    return <span style={{color:"yellow"}}>Colored Component</span>
+}
+
+
+
+
 const Tt = () => {
   return (
     <div>
         Tool tip
+        <div>
         <Tippy content='Basic Tooltip'>
         <button>Hover</button>
 
         </Tippy>
+
+        </div>
+
+        <div style={{paddingBottom:"20px"}}>
+            <Tippy content={<span style={{color:'orange'}}>Colored</span>}>
+                <button >Hover</button>
+            </Tippy>
+        </div>
         
+        
+        <div style={{paddingBottom:"20px"}}>
+            <Tippy content={<ColoredTooltip/>}>
+                <button >Hover</button>
+            </Tippy>
+        </div>
     </div>
   )
 }
