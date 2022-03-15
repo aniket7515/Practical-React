8819625d141d75cb3206423366b4eca0b183ef29
Aniket <aniketdhokane2071@gmail.com>
2022-03-15 17:03:17 +0530

    react credit cards setup

---
+++ src/components/Cc.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from 'react-credit-cards'
+import './App.css'
+import 'react-credit-cards/es/styles-compiled.css'
+
+const Cc = () => {
+  return (
+    <div>Cc</div>
+  )
+}
+
+export default Cc
